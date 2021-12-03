19c5f9fa3f17b19361782f61cf72ff5000ce141b
eltonpellata <eltonjeremy46@gmail.com>
2021-12-03 06:52:48 -0800

    first commit

---
+++ assets/js/index-charts.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/* Chart.js docs: https://www.chartjs.org/ */
+
+window.chartColors = {
+	green: '#75c181',
+	gray: '#a9b5c9',
+	text: '#252930',
+	border: '#e7e9ed'
+};
+
+/* Random number generator for demo purpose */
+var randomDataPoint = function(){ return Math.round(Math.random()*10000)};
+
+
+
+// Chart.js Bar Chart Example 
+
+var barChartConfig = {
+	type: 'bar',
+
+	data: {
+		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+		datasets: [{
+			label: 'Orders',
+			backgroundColor: window.chartColors.green,
+			borderColor: window.chartColors.green,
+			borderWidth: 1,
+			maxBarThickness: 16,
+			
+			data: [
+				23,
+				45,
+				76,
+				75,
+				62,
+				37,
+				83
+			]
+		}]
+	},
+	options: {
+		responsive: true,
+		aspectRatio: 1.5,
+		legend: {
+			position: 'bottom',
+			align: 'end',
+		},
+		title: {
+			display: true,
+			text: 'Chart.js Bar Chart Example'
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.borders,
+				},
+
+				
+			}]
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+// Generate charts on load
+window.addEventListener('load', function(){
+	
+	var barChart = document.getElementById('canvas-barchart').getContext('2d');
+	window.myBar = new Chart(barChart, barChartConfig);
+	
+
+});	
+	
